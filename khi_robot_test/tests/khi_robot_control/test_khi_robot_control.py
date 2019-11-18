af63c1c414c8aa94c0061482e2400b8e96d2f2f0
d-nakamichi <nakamichi_d@khi.co.jp>
2019-11-18 14:32:05 +0900

    Move tests from khi_robot_control to khi_robot_test

--- khi_robot_test/tests/khi_robot_control/test_khi_robot_control.py
+++ khi_robot_test/tests/khi_robot_control/test_khi_robot_control.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python
 # coding:utf-8
-##########################################
-# @file		position_velocity_test.py
-# @brief	each joint move
-# @author	matsui_hiro, d-nakamichi
-# @date		2019/7/9
-##########################################
 
-PKG = 'khi_robot_control'
+# Software License Agreement (BSD License)
+#
+#  Copyright (c) 2019, Kawasaki Heavy Industries, LTD.
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above
+#     copyright notice, this list of conditions and the following
+#     disclaimer in the documentation and/or other materials provided
+#     with the distribution.
+#   * Neither the name of the Willow Garage nor the names of its
+#     contributors may be used to endorse or promote products derived
+#     from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+#  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+
+PKG = 'khi_robot_test'
 import roslib; roslib.load_manifest(PKG)
 
 import copy
@@ -19,15 +45,18 @@ import moveit_commander
 import geometry_msgs.msg
 from khi_robot_msgs.srv import *
 
-service = 'khi_robot_command_service'
-sender = 'moveit_command_sender'
+if rospy.has_param('/test_group_name'):
+    gn = '/' + rospy.get_param('/test_group_name')
+else:
+    gn = ''
+service = gn+'/khi_robot_command_service'
 planner = 'RRTConnectkConfigDefault'
 
 class KhiRobot:
     arm_name = ''
     arm_num = 1
     max_jt = 6
-    group = 'manipulator'
+    group = gn+'/manipulator'
     min_pos_list = []
     max_pos_list = []
     max_vel_list = []
@@ -39,8 +68,8 @@ class KhiRobot:
         self.max_pos_list = []
         self.max_vel_list = []
         self.max_acc_list = []
-        self.arm_name = rospy.get_param('/khi_robot_controllers/robot')
-        limits = rospy.get_param('/'+self.arm_name+'/joint_limits')
+        self.arm_name = rospy.get_param(gn+'/khi_robot_controllers/robot')
+        limits = rospy.get_param(gn+'/'+self.arm_name+'/joint_limits')
         if self.arm_name == 'WD002N':
             self.arm_num = 2
             self.max_jt = 8
@@ -124,7 +153,6 @@ def plan_and_execute(mgc,jt,num,timeout):
     return False
 
 class TestKhiRobotControl(unittest.TestCase):
-
     def test_position_velocity(self):
         ##############################set parameter############################
         accuracy_jt  = 0.01    # joint accuracy
@@ -142,7 +170,6 @@ class TestKhiRobotControl(unittest.TestCase):
         ########################################################################
 
         ######################################### set range of move #####################################################
-        rospy.init_node(sender)
         khi_robot = KhiRobot()
 
         # RobotCommander
@@ -174,9 +201,7 @@ class TestKhiRobotControl(unittest.TestCase):
                     ano = 1
             else:
                 ano = 0
-    
-            fname = 'r%d_maxsp_jt%s_v%d_a%d' % (ano+1,jt+1,max_vel*100, max_acc*100)
-    
+
             # min position/velocity/acceleration
             mgc.set_max_velocity_scaling_factor(min_vel)
             mgc.set_max_acceleration_scaling_factor(min_acc)
@@ -188,7 +213,7 @@ class TestKhiRobotControl(unittest.TestCase):
             # max velocity/acceleration
             mgc.set_max_velocity_scaling_factor(max_vel)
             mgc.set_max_acceleration_scaling_factor(max_acc)
-    
+
             # do repeat moving
             for num in range(cyc_num):
                 # max position
@@ -228,8 +253,24 @@ class TestKhiRobotControl(unittest.TestCase):
             if ret.cmd_ret != 'ACTIVE':
                 break
 
-        cmdhandler_client('driver' , 'quit')
+    def test_state(self):
+        cmdhandler_client('driver', 'restart')
+        rospy.sleep(3)
+        ret = get_driver_state()
+        self.assertEqual('ACTIVE', ret.cmd_ret)
+
+        cmdhandler_client('driver', 'hold')
+        rospy.sleep(3)
+        ret = get_driver_state()
+        self.assertEqual('HOLDED', ret.cmd_ret)
+
+        cmdhandler_client('driver', 'restart')
+        rospy.sleep(3)
+        ret = get_driver_state()
+        self.assertEqual('ACTIVE', ret.cmd_ret)
 
 if __name__ == '__main__':
     import rostest
+    cmdhandler_client('driver', 'restart')
+    rospy.init_node("test_khi_robot_control_node")
     rostest.rosrun(PKG, 'test_khi_robot_control', TestKhiRobotControl)
